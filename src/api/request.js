8e765be5d9d55d505aa4b78511af607a53dae0f9
zhazhahui <1376393028@qq.com>
2022-01-04 18:04:56 +0800

    request封装

---
+++ src/api/request.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+import { Message } from "element-ui"
+
+axios.defaults.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.baseUrl = '//blog-server.hunger-valley.com'
+
+export default function request(url,type="GET",data={}) {
+    let option = {
+        url,
+        method:type,
+    }
+    if(type.toLowerCase() == 'get') {
+        option.params = data
+    }else {
+        option.data = data
+    }
+
+    if(localStorage.token) {
+        axios.defaults.headers.common['Authorization'] = localStorage.token
+    }
+    axios(option).then(res=>{
+        console.log("res",res)
+        if(res.data.status == 'ok') {
+            if(res.data.token) {
+                localStorage.token = res.data.token
+            }
+            resolve(res.data)
+        }else {
+            Message.error(res.data.msg)
+            reject(res.data)
+        }
+    }).catch(err=>{
+        Message.error('网络异常')
+        reject({msg:'网络异常'}) 
+    })
+}
